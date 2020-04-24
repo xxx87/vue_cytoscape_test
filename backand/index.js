f4b18fd3cad793fed52dae1616058d8a1014e2a5
i.isaev <i.isaev@pronet.ua>
2020-04-24 15:00:41 +0300

    CR

---
+++ backand/index.js
@@ -0,0 +1,144 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "sky",
+  host: "localhost",
+  database: "forBPMN",
+  password: "passwOrd",
+  port: 5432
+});
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+app.get("/nodes/:routecode", async (request, response) => {
+  const rCode = request.params.routecode;
+
+  let responseArray = [];
+
+  let nodes = (
+    await pool.query(
+      `SELECT id, code, blocktype, "xPos", "yPos" FROM wfnode WHERE "wftypicalrouteRef" = '${rCode}'`
+    )
+  )["rows"];
+
+  let edges = (
+    await pool.query(
+      `SELECT id, code, "wfnode1_code", "wfnode2_code", "outVariantKey", "outCondition" FROM wflink WHERE "wftypicalrouteRef" = '${rCode}'`
+    )
+  )["rows"];
+
+  let nodesObj = nodes.map((item) => {
+    return {
+      data: {
+        id: item.id,
+        code: item.code,
+        name: "Node_" + item.id,
+        nodeType: item.blocktype,
+        maxCharInRow: 20,
+        textMarginY: 15,
+        shape: "roundrectangle",
+        width: 150,
+        height: 100,
+        color: "grey",
+        textOutlineColor: "#bcbcbc",
+        textColor: "black",
+        image: "../bp/NodeTask.png",
+        imagePositionX: "50%",
+        imagePositionY: "0",
+        imageWidth: "40",
+        imageHeight: "40",
+        imageFitType: "none"
+      },
+      position: { x: parseInt(item.xPos), y: parseInt(item.yPos) }
+    };
+  });
+
+// console.log(nodesObj.find(node => node.data.code === "node154324447273926").data.id);
+
+  let edgesObj = edges.map((item) => {
+    return {
+      data: {
+        id: item.id,
+        code: item.code,
+        name: "Edge_" + item.id,
+        source: nodesObj.find(node => node.data.code === item.wfnode1_code).data.id,
+        target: nodesObj.find(node => node.data.code === item.wfnode2_code).data.id,
+        // source: nodesObj.find(node => node.code === item.wfnode1_code).id,
+        // target: nodesObj.find(node => node.code === item.wfnode2_code).id,
+        outVariantKey: item.outVariantKey,
+        outCondition: item.outCondition
+      }
+    };
+  });
+
+
+  responseArray = nodesObj.concat(edgesObj);
+  response.status(200).json(responseArray);
+  // console.log(nodes);
+  //   ,
+  //   (error, results) => {
+  //     if (error) {
+  //       throw error;
+  //     }
+  //     // console.log(results.rows);
+  //     let newData = results.rows.map((item) => {
+  //       return {
+  //         data: {
+  //           id: item.id,
+  //           code: item.code,
+  //           name: "Node_" + item.id,
+  //           nodeType: item.blocktype,
+  //           maxCharInRow: 20,
+  //           textMarginY: 15,
+  //           shape: "roundrectangle",
+  //           width: 150,
+  //           height: 100,
+  //           color: "grey",
+  //           textOutlineColor: "#bcbcbc",
+  //           textColor: "black",
+  //           image: "../bp/NodeTask.png",
+  //           imagePositionX: "50%",
+  //           imagePositionY: "0",
+  //           imageWidth: "40",
+  //           imageHeight: "40",
+  //           imageFitType: "none"
+  //         },
+  //         position: { x: parseInt(item.xPos), y: parseInt(item.yPos) }
+  //       };
+  //     });
+  //     // console.log(newData);
+  //     response.status(200).json(newData);
+  //   }
+  // );
+});
+app.get("/edges/:routecode", (request, response) => {
+  const rCode = request.params.routecode;
+  pool.query(
+    `SELECT * FROM wflink WHERE "wftypicalrouteRef" = '${rCode}'`,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
--- src/App.vue
+++ src/App.vue
@@ -10,31 +10,39 @@
         v-for="def in elements"
         :key="`${def.data.id}`"
         :definition="def"
-        :style="`${def.data.type}`"
       />
     </cytoscape>
   </div>
 </template>
 <script>
 // import $ from "jquery";
+import axios from "axios";
 import cytoscape from "@/plugins/cytoscape";
-import elements from "./elements";
+// import elements from "./elements";
 import config from "./example-config";
 
 export default {
   name: "HelloWorld",
   data: function() {
     return {
-      config,
-      elements
+      elements: [],
+      config
     };
   },
-  methods: {
-    preConfig() {
-
-    },
-    afterCreated() {}
+  created() {
+    console.log(this.config);
+    axios
+      .get("http://localhost:3000/nodes/76a8d2db-ffd6-4345-a8fc-fb71613a4b19")
+      .then((resp) => {
+        this.elements = resp.data;
+        console.log(this.elements);
+      })
+      .catch((e) => console.log(e));
   },
+  methods: {
+    preConfig() {},
+    afterCreated() {}
+  }
 };
 </script>
 
